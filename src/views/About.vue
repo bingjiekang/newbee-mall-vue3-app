1ff473815f9c512c1922ef69ceeb58eb0cd6a2f8
kangbingjie <69076498+bingjiekang@users.noreply.github.com>
2024-03-26 16:19:27 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,7 +12,7 @@
     <div class="about-body">
       <img class="intro-img" src="/static-files/商城info.jpeg" alt="">
       <van-divider :style="{ color: '#1baeae', borderColor: '#1baeae', fontSize: '20px', fontWeight: 500 }">简介</van-divider>
-      <div>区块链商城项目是一套电商系统，包括区块链商城系统及区块链商城后台管理系统，区块链商城系统基于 Spring Boot 3.X 、百度超级链、Gin框架(Golang)等相关技术栈开发。 前台商城系统包含首页门户、商品分类、新品上线、首页轮播、商品推荐、商品搜索、商品展示、购物车、订单结算、订单流程、个人订单管理、会员中心、帮助中心等模块。 后台管理系统包含数据面板、轮播图管理、商品管理、订单管理、会员管理、分类管理、设置等模块。</div>
+      <div>区块链商城项目是一套电商系统，包括区块链商城系统及区块链商城后台管理系统，区块链商城系统基于 Spring Boot 3.X 、Gin框架(Golang)等相关技术栈开发。 前台商城系统包含首页门户、商品分类、新品上线、首页轮播、商品推荐、商品搜索、商品展示、购物车、订单结算、订单流程、个人订单管理、会员中心、帮助中心等模块。 后台管理系统包含数据面板、轮播图管理、商品管理、订单管理、会员管理、分类管理、设置等模块。</div>
       <van-divider :style="{ color: '#1baeae', borderColor: '#1baeae', fontSize: '20px', fontWeight: 500 }">开源地址</van-divider>
       <div>
         <div>后台管理系统：<a target="_blank" href="https://juejin.im/book/6844733826191589390">《Vue 商城项目开发实战》</a></div>
