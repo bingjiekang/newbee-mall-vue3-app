004f66804c503e3d779357bfe5699859ab673be1
kangbingjie <69076498+bingjiekang@users.noreply.github.com>
2024-04-03 13:16:05 +0800

    update

---
+++ dist/assets/good-1e1f36fa.js
@@ -0,0 +1,1 @@
+import{z as e}from"./index-46517163.js";function a(t){return e.get(`/goods/detail/${t}`)}function o(){return e.get("/categories")}function s(t){return e.get("/search",{params:t})}export{a,o as g,s};
--- dist/assets/good-ba4613d5.js
+++
@@ -1,1 +0,0 @@
-import{z as e}from"./index-7f6e4180.js";function a(t){return e.get(`/goods/detail/${t}`)}function o(){return e.get("/categories")}function s(t){return e.get("/search",{params:t})}export{a,o as g,s};
